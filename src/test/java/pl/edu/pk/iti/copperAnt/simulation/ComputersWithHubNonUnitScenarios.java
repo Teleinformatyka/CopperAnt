403d28d9fef2a16cd3007b4077e48f8391b78703
Michał Jaworek <jaworekmichal@gmail.com>
2015-01-10 11:23:39 +0100

    test for hub contains hub

--- src/test/java/pl/edu/pk/iti/copperAnt/simulation/ComputersWithHubNonUnitScenarios.java
+++ src/test/java/pl/edu/pk/iti/copperAnt/simulation/ComputersWithHubNonUnitScenarios.java
@@ -8,8 +8,10 @@ import static org.mockito.Mockito.verify;
 import org.junit.Test;
 import org.mockito.ArgumentCaptor;
 
+import pl.edu.pk.iti.copperAnt.TestUtils;
 import pl.edu.pk.iti.copperAnt.network.Cable;
 import pl.edu.pk.iti.copperAnt.network.Computer;
+import pl.edu.pk.iti.copperAnt.network.Hub;
 import pl.edu.pk.iti.copperAnt.network.IPAddress;
 import pl.edu.pk.iti.copperAnt.network.Package;
 import pl.edu.pk.iti.copperAnt.network.PackageType;
@@ -25,9 +27,13 @@ public class ComputersWithHubNonUnitScenarios {
 		computer1.setPort(spy(computer1.getPort()));
 		Computer computer2 = new Computer(new IPAddress("192.168.1.2"));
 		computer2.setPort(spy(computer2.getPort()));
-		Cable cable = spy(new Cable());
-		cable.insertInto(computer1.getPort());
-		cable.insertInto(computer2.getPort());
+		Hub hub = new Hub(2);
+		Cable cable1 = new Cable();
+		cable1.insertInto(hub.getPort(0));
+		cable1.insertInto(computer1.getPort());
+		Cable cable2 = new Cable();
+		cable2.insertInto(hub.getPort(1));
+		cable2.insertInto(computer2.getPort());
 		Package pack = new Package();
 		pack.setDestinationIP(computer2.getIP());
 
@@ -39,14 +45,17 @@ public class ComputersWithHubNonUnitScenarios {
 		verify(computer1.getPort(), atLeastOnce()).receivePackage(
 				packageCaptor.capture());
 
-		Package echoRq = packageCaptor.getAllValues().get(1);
-
-		assertEquals(PackageType.ECHO_REPLY, echoRq.getType());
-		assertEquals(computer1.getPort().getMAC(), echoRq.getDestinationMAC());
-		assertEquals(computer1.getIP(), echoRq.getDestinationIP());
-		assertEquals(computer2.getPort().getMAC(), echoRq.getSourceMAC());
-		assertEquals(computer2.getIP(), echoRq.getSourceIP());
+		boolean echoReplyWasReceived = packageCaptor//
+				.getAllValues()//
+				.stream()
+				//
+				.anyMatch(p -> (TestUtils.checkExpectedParametersOfPackage(p,//
+						computer2.getPort().getMAC(),//
+						computer2.getIP(),//
+						computer1.getPort().getMAC(),//
+						computer1.getIP(),//
+						PackageType.ECHO_REPLY)));
+		assertTrue(echoReplyWasReceived);
 
 	}
-
 }

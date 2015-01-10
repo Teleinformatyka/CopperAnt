705597e64973b977845ae004211e79a9596b3900
Yuriy Ganushevich <gan.yuriy@gmail.com>
2015-01-10 20:11:23 +0100

    green diode turnoff on start

--- src/main/java/pl/edu/pk/iti/copperAnt/gui/PortControl.java
+++ src/main/java/pl/edu/pk/iti/copperAnt/gui/PortControl.java
@@ -53,7 +53,7 @@ public class PortControl extends Control {
 		redDiode.turnOn();
 
 		greenDiode = new DiodeControl(width / 2, height / 10, Color.GREENYELLOW);
-		greenDiode.turnOn();
+		//greenDiode.turnOn();
 
 		getChildren().add(redDiode);
 		getChildren().add(greenDiode);
